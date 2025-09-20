6cc78764ba04ff58f10826e02fa4b3baa2c91c7e
Shine-Ritas <whoishsh@gmail.com>
2025-09-21 00:20:53 +0700

    dev : code update

--- deployment/docker/Dockerfile.php
+++ deployment/docker/Dockerfile.php
@@ -98,7 +98,7 @@ RUN composer install --optimize-autoloader  \
 
 EXPOSE 9001
 
-CMD ["php-fpm", "-y", "/usr/local/etc/php-fpm.d/custom-php-fpm.conf", "-R"]
+CMD ["php-fpm", "-y", "/usr/local/etc/php-fpm.d/www.conf", "-R"]
 
 FROM template AS worker
 

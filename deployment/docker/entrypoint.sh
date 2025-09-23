4695b7e68272587f59844d12183e8fa30e1f864f
Shine-Ritas <whoishsh@gmail.com>
2025-09-23 19:50:11 +0700

    dev : code

--- deployment/docker/entrypoint.sh
+++ deployment/docker/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
+umask 0002   # must be first, before anything else
 set -e
-
-# Set umask so new files/folders are group-writable
-umask 0002
 
 # Ensure storage and cache have proper permissions
 mkdir -p /var/www/mgn/storage /var/www/mgn/bootstrap/cache
 chown -R www-data:www-data /var/www/mgn/storage /var/www/mgn/bootstrap/cache
 chmod -R 775 /var/www/mgn/storage /var/www/mgn/bootstrap/cache
+
+# Debug: print umask so we know it's set correctly
+umask
 
 # Start main process
 exec "$@"
